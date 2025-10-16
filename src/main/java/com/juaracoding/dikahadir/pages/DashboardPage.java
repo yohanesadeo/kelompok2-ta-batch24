ccc29eef900b2df640ffde65003988565e84e8b0
Yohanes Adeo <82396456+yohanesadeo@users.noreply.github.com>
2025-10-16 19:55:18 +0700

    xpath dashboard done(?)

--- src/main/java/com/juaracoding/dikahadir/pages/DashboardPage.java
+++ src/main/java/com/juaracoding/dikahadir/pages/DashboardPage.java
@@ -3,9 +3,30 @@ package com.juaracoding.dikahadir.pages;
 import org.openqa.selenium.By;
 
 public class DashboardPage {
-    //Locator
-    private By buttonMenuManagement = By.xpath("//div[contains(@class, 'sidebar__wrapper')][.//p[text()='Management']]");
-    private By buttonMenuUser = By.xpath("//div[contains(@class, 'sidebar__wrapper')][.//p[text()='User']]");
+    // Main Menus
+    private By buttonMenuManagement = By
+            .xpath("//p[text()='Management']/ancestor::div[contains(@class, 'sidebar__wrapper')]");
+    private By buttonMenuLaporan = By
+            .xpath("//p[text()='Laporan']/ancestor::div[contains(@class, 'sidebar__wrapper')]");
+
+    // Management Sub-Menus
+    private By subMenuPosisi = By.xpath("//div[contains(@class, 'MuiCollapse-root')]//p[text()='Posisi']");
+    private By subMenuAbsenPoint = By.xpath("//div[contains(@class, 'MuiCollapse-root')]//p[text()='Absen Point']");
+    private By subMenuJabatan = By.xpath("//div[contains(@class, 'MuiCollapse-root')]//p[text()='Jabatan']");
+    private By subMenuShifting = By.xpath("//div[contains(@class, 'MuiCollapse-root')]//p[text()='Shifting']");
+    private By subMenuJadwal = By.xpath("//div[contains(@class, 'MuiCollapse-root')]//p[text()='Jadwal']");
+    private By subMenuKalender = By.xpath("//div[contains(@class, 'MuiCollapse-root')]//p[text()='Kalender']");
+    private By subMenuDayOff = By.xpath("//div[contains(@class, 'MuiCollapse-root')]//p[text()='Day Off']");
+
+    // Laporan Sub-Menus
+    private By subMenuLaporanSemua = By.xpath("//div[contains(@class, 'MuiCollapse-root')]//p[text()='Semua']");
+    private By subMenuLaporanKehadiran = By.xpath("//div[contains(@class, 'MuiCollapse-root')]//p[text()='Kehadiran']");
+    private By subMenuLaporanIzinTerlambat = By
+            .xpath("//div[contains(@class, 'MuiCollapse-root')]//p[text()='Izin Terlambat']");
+    private By subMenuLaporanPulangCepat = By
+            .xpath("//div[contains(@class, 'MuiCollapse-root')]//p[text()='Pulang Cepat']");
+
+    // Elements inside Laporan pages (example for search)
     private By searchBar = By.xpath("//input[@id='search']");
     private By searchButton = By.xpath("//button[normalize-space()='Search']");
 
