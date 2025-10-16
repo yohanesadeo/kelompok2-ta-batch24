9ebe24b493e0f69d7a7d7ec6c45f7160b571e0fa
Yohanes Adeo <82396456+yohanesadeo@users.noreply.github.com>
2025-10-16 20:54:40 +0700

    done dashboard

--- src/main/java/com/juaracoding/dikahadir/pages/DashboardPage.java
+++ src/main/java/com/juaracoding/dikahadir/pages/DashboardPage.java
@@ -1,8 +1,11 @@
 package com.juaracoding.dikahadir.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class DashboardPage {
+    private WebDriver driver;
+
     // Main Menus
     private By buttonMenuManagement = By
             .xpath("//p[text()='Management']/ancestor::div[contains(@class, 'sidebar__wrapper')]");
@@ -26,8 +29,99 @@ public class DashboardPage {
     private By subMenuLaporanPulangCepat = By
             .xpath("//div[contains(@class, 'MuiCollapse-root')]//p[text()='Pulang Cepat']");
 
-    // Elements inside Laporan pages (example for search)
-    private By searchBar = By.xpath("//input[@id='search']");
+    // Elemen Umum
+    private By searchBar = By.xpath("//input[@placeholder='Search']");
     private By searchButton = By.xpath("//button[normalize-space()='Search']");
 
+    // Kontrol Halaman (Pagination)
+    private By nextPageButton = By.xpath("//button[@aria-label='Go to next page']");
+    private By previousPageButton = By.xpath("//button[@aria-label='Go to previous page']");
+   
+    private By specificPageButton(int pageNumber) {
+        return By.xpath("//button[@aria-label='Go to page " + pageNumber + "']");
+    }
+
+    private By rowsPerPageDropdown = By
+            .xpath("//div[contains(@class, 'MuiTablePagination-selectLabel')]/following-sibling::div");
+
+    public DashboardPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickButtonMenuManagement() {
+        driver.findElement(buttonMenuManagement).click();
+    }
+
+    public void clickSubMenuPosisi() {
+        driver.findElement(subMenuPosisi).click();
+    }
+
+    public void clickSubMenuAbsenPoint() {
+        driver.findElement(subMenuAbsenPoint).click();
+    }
+
+    public void clickSubMenuJabatan() {
+        driver.findElement(subMenuJabatan).click();
+    }
+
+    public void clickSubMenuShifting() {
+        driver.findElement(subMenuShifting).click();
+    }
+
+    public void clickSubMenuJadwal() {
+        driver.findElement(subMenuJadwal).click();
+    }
+
+    public void clickSubMenuKalender() {
+        driver.findElement(subMenuKalender).click();
+    }
+
+    public void clickSubMenuDayOff() {
+        driver.findElement(subMenuDayOff).click();
+    }
+
+    public void clickButtonMenuLaporan() {
+        driver.findElement(buttonMenuLaporan).click();
+    }
+
+    public void clickSubMenuLaporanSemua() {
+        driver.findElement(subMenuLaporanSemua).click();
+    }
+
+    public void clickSubMenuLaporanKehadiran() {
+        driver.findElement(subMenuLaporanKehadiran).click();
+    }
+
+    public void clickSubMenuLaporanIzinTerlambat() {
+        driver.findElement(subMenuLaporanIzinTerlambat).click();
+    }
+
+    public void clickSubMenuLaporanPulangCepat() {
+        driver.findElement(subMenuLaporanPulangCepat).click();
+    }
+
+    public void fillSearchBar(String text) {
+        driver.findElement(searchBar).sendKeys(text);
+    }
+
+    public void clickSearchButton() {
+        driver.findElement(searchButton).click();
+    }
+
+    public void clickNextPageButton() {
+        driver.findElement(nextPageButton).click();
+    }
+
+    public void clickPreviousPageButton() {
+        driver.findElement(previousPageButton).click();
+    }
+
+    public void clickSpecificPageButton(int pageNumber) {
+        driver.findElement(specificPageButton(pageNumber)).click();
+    }
+
+    public void selectRowsPerPage(String rowsPerPage) {
+        driver.findElement(rowsPerPageDropdown).click();
+        driver.findElement(By.xpath("//li[text()='" + rowsPerPage + "']")).click();
+    }
 }
